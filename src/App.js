9fea8914386007ab2a9c9eb61e4073f527492336
limeojin <2002eojin@gmail.com>
2022-09-05 23:51:35 +0900

    Fuck

--- src/App.js
+++ src/App.js
@@ -6,24 +6,7 @@ function App() {
   
   return (
     <div className="App">
-      <div className="navBar">
-        <div className="navBack" onClick={()=>{navigate(-1)}}>
-          &#60;	
-        </div>
-        <span className="navTitle" onClick={()=>{navigate('/')}}>
-          <span className="mainTitle">1만 시간의 법칙</span>
-          <span className="subTitle">The law of 10,000 hours</span>
-        </span>
-        <div className="navGo" onClick={()=>{navigate(1)}}>
-          &#62;	
-        </div>
-      </div>
-      <div className="AppMain">
-        <Routes>
-          <Route path='/' element={<div>fasdfsd</div>}/>
-          
-        </Routes>
-      </div>
+      ads
     </div>
   );
 }
